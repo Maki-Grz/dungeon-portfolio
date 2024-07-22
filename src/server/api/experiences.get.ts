46b9413fa6b399361efcd484814d0dffb618a831
Maki-Grz <mini@ledret.ch>
2024-07-22 18:56:27 +0200

    feat(azure): deploy to SWA

--- src/server/api/experiences.get.ts
+++ src/server/api/experiences.get.ts
@@ -1,12 +1,19 @@
 export default defineEventHandler(async (event) => {
     try {
-        return await $fetch(`${process.env.API_URL}/experiences`, {
-            headers: {"Content-Type": "application/json"},
-        });
-    } catch (err) {
+        const response = await fetch(`${process.env.API_URL}/experiences`);
+
+        console.info(`Fetch status: ${response.status}`);
+
+        if (!response.ok) {
+            return new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (err: any) {
+        console.error(err.message);
         throw createError({
-            message: "Authorization Failed",
-            statusCode: 401,
+            message: "Internal Error",
+            statusCode: 500,
         });
     }
 });
